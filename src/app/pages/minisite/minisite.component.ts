4dbd4e110ddc539022b7af411c2aefa6c1565b8b
Chris Kanger <chris@3rdfloorcoding.nl>
2018-11-20 22:34:42 +0100

    implemented css grid system

--- src/app/pages/minisite/minisite.component.ts
+++ src/app/pages/minisite/minisite.component.ts
@@ -4,19 +4,9 @@ import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'vtb-minisite',
   templateUrl: './minisite.component.html',
-  styles: [`
-    agm-map {
-      width: 1028px;
-      height: 426px;
-    }
-  `],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class MinisiteComponent implements OnInit {
-
-  lat: number = 51.678418;
-  lng: number = 7.809007;
-
   itinerary: any;
 
   constructor(private _activatedRoute: ActivatedRoute) {

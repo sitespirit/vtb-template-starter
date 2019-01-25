6bc13946c388440875344fe813a24cd95166547e
Chris Kanger <chris@3rdfloorcoding.nl>
2019-01-25 16:28:37 +0100

    updated youtube code

--- src/app/resolvers/itinerary.resolver.ts
+++ src/app/resolvers/itinerary.resolver.ts
@@ -6,6 +6,6 @@ import { HttpClient } from '@angular/common/http';
 export class ItineraryResolver implements Resolve<any> {
   constructor(private http: HttpClient) {}
   resolve():Promise<any> {
-    return this.http.get('../../../assets/json/stub.json').toPromise();
+    return this.http.get('../../../assets/json/stub-boh-birds.json').toPromise();
   }
 }

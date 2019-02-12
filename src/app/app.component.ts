0ac9110dd396888013d0415927df53d96c4ed590
cmddavid <david.dam18@gmail.com>
2019-02-12 13:26:29 +0100

    Reverted changes for codesandbox

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: 'test'
+  template: '<router-outlet></router-outlet>'
 })
 export class AppComponent {
 }
